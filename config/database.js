5dea5f6864049d794b5001e28166216faa2adf59
Todd Brannon <toddqbrannon@gmail.com>
2021-09-12 10:58:33 -0500

    add DEV & PROD creds to database.js and complete ejs refs in lenderdetail.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,27 @@
 const { createPool } = require('mysql');
 
+// DEV - Azure
 const pool = createPool({
     port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.MYSQL_DB,
+    host: process.env.DB_HOST_DEV,
+    user: process.env.DB_USER_DEV,
+    password: process.env.DB_PASS_DEV,
+    database: process.env.MYSQL_DB_DEV,
     multipleStatements: true,
     ssl: true,
     connectionLimit: 10
 })
+
+// TEST - Heroku (JawsDB)
+// const pool = createPool({
+//     port: process.env.DB_PORT,
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASS,
+//     database: process.env.MYSQL_DB,
+//     multipleStatements: true,
+//     ssl: true,
+//     connectionLimit: 10
+// })
 
 module.exports = pool;
