0c27e5b7fd7f10c62e4abbc2c00a9893a58d8426
Todd Brannon <toddqbrannon@gmail.com>
2021-09-12 08:28:15 -0500

    had to change database config variable from dev to prod

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const pool = createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.MYSQL_DB_DEV,
+    database: process.env.MYSQL_DB,
     multipleStatements: true,
     ssl: true,
     connectionLimit: 10
